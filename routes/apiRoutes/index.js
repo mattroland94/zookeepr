90adfe31de5707cb4f0dc96df3283ac46d7cf7e8
Matt Roland <83994674+mattroland94@users.noreply.github.com>
2021-11-09 11:05:41 -0600

    Edited index.js for functionality

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,8 @@
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes')
 
-router.use(require('./zookeeperRoutes'));
 router.use(animalRoutes);
+router.use(zookeeperRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-const { animals } = require('./data/animals');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes')
 
-app.use(express.static('public'));
 app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
